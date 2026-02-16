a8356d865330ec11a0d92624530af1bb4adfcd97
thrust0 <147539222+thrust0@users.noreply.github.com>
2026-02-16 13:36:42 +0200

    Refactor uzduotis.cpp to remove RTF formatting

--- uzduotis.cpp
+++ uzduotis.cpp
@@ -1,133 +1,62 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2867
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;\f1\fnil\fcharset0 Menlo-Bold;\f2\fnil\fcharset0 Menlo-Italic;
+#include <iomanip>
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::string;
+using std::vector;
+using std::left;
+using std::right;
+using std::setw;
+using std::endl;
+
+struct Students {
+    string name = "A", last_name = "BB";
+    //int *paz jei dinaminis masyvas
+    vector <int> grade;
+    int exam;
+    double result;
+};
+void input(vector<Students>& group);
+void output(vector<Students>& group);
+
+int main() 
+{
+    vector<Students>group;
+    input(group);
+    output(group);
+
 }
-{\colortbl;\red255\green255\blue255;\red115\green138\blue4;\red252\green244\blue220;\red165\green119\blue5;
-\red37\green146\blue134;\red83\green104\blue112;\red189\green54\blue19;\red33\green118\blue199;\red71\green91\blue98;
-\red129\green144\blue144;\red198\green28\blue111;}
-{\*\expandedcolortbl;;\cssrgb\c52157\c60000\c0;\cssrgb\c99216\c96471\c89020;\cssrgb\c70980\c53725\c0;
-\cssrgb\c16471\c63137\c59608;\cssrgb\c39608\c48235\c51373;\cssrgb\c79608\c29412\c8627;\cssrgb\c14902\c54510\c82353;\cssrgb\c34510\c43137\c45882;
-\cssrgb\c57647\c63137\c63137;\cssrgb\c82745\c21176\c50980;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
-
-\f0\fs24 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 #include\cf4 \strokec4  \cf5 \strokec5 <iomanip>\cf6 \cb1 \strokec6 \
-\cf2 \cb3 \strokec2 #include\cf4 \strokec4  \cf5 \strokec5 <vector>\cf6 \cb1 \strokec6 \
-\cf2 \cb3 \strokec2 #include\cf4 \strokec4  \cf5 \strokec5 <iostream>\cf6 \cb1 \strokec6 \
-\cf2 \cb3 \strokec2 #include\cf4 \strokec4  \cf5 \strokec5 <string>\cf6 \cb1 \strokec6 \
-\
-\cf2 \cb3 \strokec2 using\cf6 \strokec6  \cf7 \strokec7 std\cf6 \strokec6 ::\cf8 \strokec8 cin\cf6 \strokec6 ;\cb1 \
-\cf2 \cb3 \strokec2 using\cf6 \strokec6  \cf7 \strokec7 std\cf6 \strokec6 ::\cf8 \strokec8 cout\cf6 \strokec6 ;\cb1 \
-\cf2 \cb3 \strokec2 using\cf6 \strokec6  \cf7 \strokec7 std\cf6 \strokec6 ::\cf7 \strokec7 string\cf6 \strokec6 ;\cb1 \
-\cf2 \cb3 \strokec2 using\cf6 \strokec6  \cf7 \strokec7 std\cf6 \strokec6 ::\cf7 \strokec7 vector\cf6 \strokec6 ;\cb1 \
-\cf2 \cb3 \strokec2 using\cf6 \strokec6  \cf7 \strokec7 std\cf6 \strokec6 ::\cf8 \strokec8 left\cf6 \strokec6 ;\cb1 \
-\cf2 \cb3 \strokec2 using\cf6 \strokec6  \cf7 \strokec7 std\cf6 \strokec6 ::\cf8 \strokec8 right\cf6 \strokec6 ;\cb1 \
-\cf2 \cb3 \strokec2 using\cf6 \strokec6  \cf7 \strokec7 std\cf6 \strokec6 ::\cf8 \strokec8 setw\cf6 \strokec6 ;\cb1 \
-\cf2 \cb3 \strokec2 using\cf6 \strokec6  \cf7 \strokec7 std\cf6 \strokec6 ::\cf8 \strokec8 endl\cf6 \strokec6 ;\cb1 \
-\
-\pard\pardeftab720\partightenfactor0
-
-\f1\b \cf9 \cb3 \strokec9 struct
-\f0\b0 \cf6 \strokec6  \cf7 \strokec7 Students\cf6 \strokec6  \{\cb1 \
-\pard\pardeftab720\partightenfactor0
-\cf6 \cb3     \cf7 \strokec7 string\cf6 \strokec6  \cf8 \strokec8 name\cf6 \strokec6  \cf2 \strokec2 =\cf6 \strokec6  \cf5 \strokec5 "A"\cf6 \strokec6 , \cf8 \strokec8 last_name\cf6 \strokec6  \cf2 \strokec2 =\cf6 \strokec6  \cf5 \strokec5 "BB"\cf6 \strokec6 ;\cb1 \
-\pard\pardeftab720\partightenfactor0
-
-\f2\i \cf10 \cb3 \strokec10     //int *paz jei dinaminis masyvas
-\f0\i0 \cf6 \cb1 \strokec6 \
-\pard\pardeftab720\partightenfactor0
-\cf6 \cb3     \cf7 \strokec7 vector\cf6 \strokec6  \cf2 \strokec2 <
-\f1\b \cf9 \strokec9 int
-\f0\b0 \cf2 \strokec2 >\cf6 \strokec6  \cf8 \strokec8 grade\cf6 \strokec6 ;\cb1 \
-\cb3     
-\f1\b \cf9 \strokec9 int
-\f0\b0 \cf6 \strokec6  \cf8 \strokec8 exam\cf6 \strokec6 ;\cb1 \
-\cb3     
-\f1\b \cf9 \strokec9 double
-\f0\b0 \cf6 \strokec6  \cf8 \strokec8 result\cf6 \strokec6 ;\cb1 \
-\cb3 \};\cb1 \
-\pard\pardeftab720\partightenfactor0
-
-\f1\b \cf9 \cb3 \strokec9 void
-\f0\b0 \cf6 \strokec6  \cf8 \strokec8 input\cf6 \strokec6 (\cf7 \strokec7 vector\cf6 \strokec6 <\cf7 \strokec7 Students\cf6 \strokec6 >
-\f1\b \cf9 \strokec9 &
-\f0\b0 \cf6 \strokec6  group);\cb1 \
-
-\f1\b \cf9 \cb3 \strokec9 void
-\f0\b0 \cf6 \strokec6  \cf8 \strokec8 output\cf6 \strokec6 (\cf7 \strokec7 vector\cf6 \strokec6 <\cf7 \strokec7 Students\cf6 \strokec6 >
-\f1\b \cf9 \strokec9 &
-\f0\b0 \cf6 \strokec6  group);\cb1 \
-\
-
-\f1\b \cf9 \cb3 \strokec9 int
-\f0\b0 \cf6 \strokec6  \cf8 \strokec8 main\cf6 \strokec6 () \cb1 \
-\pard\pardeftab720\partightenfactor0
-\cf6 \cb3 \{\cb1 \
-\cb3     \cf7 \strokec7 vector\cf2 \strokec2 <\cf7 \strokec7 Students\cf2 \strokec2 >\cf8 \strokec8 group\cf6 \strokec6 ;\cb1 \
-\cb3     \cf8 \strokec8 input\cf6 \strokec6 (\cf8 \strokec8 group\cf6 \strokec6 );\cb1 \
-\cb3     \cf8 \strokec8 output\cf6 \strokec6 (\cf8 \strokec8 group\cf6 \strokec6 );\cb1 \
-\
-\cb3 \}\cb1 \
-\pard\pardeftab720\partightenfactor0
-
-\f1\b \cf9 \cb3 \strokec9 void
-\f0\b0 \cf6 \strokec6  \cf8 \strokec8 input\cf6 \strokec6 (\cf7 \strokec7 vector\cf6 \strokec6 <\cf7 \strokec7 Students\cf6 \strokec6 >
-\f1\b \cf9 \strokec9 &
-\f0\b0 \cf6 \strokec6  group) \cb1 \
-\pard\pardeftab720\partightenfactor0
-\cf6 \cb3 \{\cb1 \
-\cb3     \cf2 \strokec2 for\cf6 \strokec6  (
-\f1\b \cf9 \strokec9 int
-\f0\b0 \cf6 \strokec6  \cf8 \strokec8 ii\cf6 \strokec6  \cf2 \strokec2 =\cf6 \strokec6  \cf11 \strokec11 0\cf6 \strokec6 ; \cf8 \strokec8 ii\cf6 \strokec6  \cf2 \strokec2 <\cf6 \strokec6  \cf11 \strokec11 2\cf6 \strokec6 ; \cf8 \strokec8 ii\cf2 \strokec2 ++\cf6 \strokec6 ) \{\cb1 \
-\cb3         \cf7 \strokec7 Students\cf6 \strokec6  \cf8 \strokec8 student\cf6 \strokec6 ;\cb1 \
-\cb3         \cf8 \strokec8 cout\cf6 \strokec6  \cf8 \strokec8 <<\cf6 \strokec6  \cf5 \strokec5 "Iveskite varda ir pavarde: "\cf6 \strokec6 ;\cb1 \
-\cb3         \cf8 \strokec8 cin\cf6 \strokec6  \cf8 \strokec8 >>\cf6 \strokec6  \cf8 \strokec8 student\cf6 \strokec6 .\cf8 \strokec8 name\cf6 \strokec6  \cf8 \strokec8 >>\cf6 \strokec6  \cf8 \strokec8 student\cf6 \strokec6 .\cf8 \strokec8 last_name\cf6 \strokec6 ;\cb1 \
-\cb3         \cf8 \strokec8 cout\cf6 \strokec6  \cf8 \strokec8 <<\cf6 \strokec6  \cf5 \strokec5 "Iveskite semestro ivertinimus. Kiek ju bus? "\cf6 \strokec6 ;\cb1 \
-\cb3         
-\f1\b \cf9 \strokec9 int
-\f0\b0 \cf6 \strokec6  \cf8 \strokec8 n\cf6 \strokec6 , \cf8 \strokec8 sum\cf6 \strokec6  \cf2 \strokec2 =\cf6 \strokec6  \cf11 \strokec11 0\cf6 \strokec6 ;\cb1 \
-\cb3         \cf8 \strokec8 cin\cf6 \strokec6  \cf8 \strokec8 >>\cf6 \strokec6  \cf8 \strokec8 n\cf6 \strokec6 ;\cb1 \
-\cb3         \cf2 \strokec2 for\cf6 \strokec6  (
-\f1\b \cf9 \strokec9 int
-\f0\b0 \cf6 \strokec6  \cf8 \strokec8 i\cf6 \strokec6  \cf2 \strokec2 =\cf6 \strokec6  \cf11 \strokec11 0\cf6 \strokec6 ; \cf8 \strokec8 i\cf6 \strokec6  \cf2 \strokec2 <\cf6 \strokec6  \cf8 \strokec8 n\cf6 \strokec6 ; \cf8 \strokec8 i\cf2 \strokec2 ++\cf6 \strokec6 ) \cb1 \
-\cb3         \{\cb1 \
-\cb3             
-\f1\b \cf9 \strokec9 int
-\f0\b0 \cf6 \strokec6  \cf8 \strokec8 temp\cf6 \strokec6 ;\cb1 \
-\cb3             \cf8 \strokec8 cout\cf6 \strokec6  \cf8 \strokec8 <<\cf6 \strokec6  \cf5 \strokec5 "Iveskite "\cf6 \strokec6  \cf8 \strokec8 <<\cf6 \strokec6  \cf8 \strokec8 i\cf6 \strokec6  \cf2 \strokec2 +\cf6 \strokec6  \cf11 \strokec11 1\cf6 \strokec6  \cf8 \strokec8 <<\cf6 \strokec6  \cf5 \strokec5 " pazymi is "\cf6 \strokec6  \cf8 \strokec8 <<\cf6 \strokec6  \cf8 \strokec8 n\cf6 \strokec6  \cf8 \strokec8 <<\cf6 \strokec6  \cf5 \strokec5 ":"\cf6 \strokec6 ;\cb1 \
-\cb3             \cf8 \strokec8 cin\cf6 \strokec6  \cf8 \strokec8 >>\cf6 \strokec6  \cf8 \strokec8 temp\cf6 \strokec6 ;\cb1 \
-\cb3             \cf8 \strokec8 student\cf6 \strokec6 .\cf8 \strokec8 grade\cf6 \strokec6 .\cf8 \strokec8 push_back\cf6 \strokec6 (\cf8 \strokec8 temp\cf6 \strokec6 ); \cf8 \strokec8 sum\cf6 \strokec6  \cf2 \strokec2 +=\cf6 \strokec6  \cf8 \strokec8 temp\cf6 \strokec6 ;\cb1 \
-\cb3         \}\cb1 \
-\cb3         \cf8 \strokec8 cout\cf6 \strokec6  \cf8 \strokec8 <<\cf6 \strokec6  \cf5 \strokec5 "Iveskite egzamina: "\cf6 \strokec6 ; \cf8 \strokec8 cin\cf6 \strokec6  \cf8 \strokec8 >>\cf6 \strokec6  \cf8 \strokec8 student\cf6 \strokec6 .\cf8 \strokec8 exam\cf6 \strokec6 ;\cb1 \
-\cb3         \cf8 \strokec8 student\cf6 \strokec6 .\cf8 \strokec8 result\cf6 \strokec6  \cf2 \strokec2 =\cf6 \strokec6  \cf8 \strokec8 sum\cf6 \strokec6  \cf2 \strokec2 *\cf6 \strokec6  \cf11 \strokec11 1.0\cf6 \strokec6  \cf2 \strokec2 /\cf6 \strokec6  (\cf8 \strokec8 n\cf6 \strokec6  \cf2 \strokec2 *\cf6 \strokec6  \cf11 \strokec11 1.0\cf6 \strokec6 ) \cf2 \strokec2 *\cf6 \strokec6  \cf11 \strokec11 0.4\cf6 \strokec6  \cf2 \strokec2 +\cf6 \strokec6  \cf8 \strokec8 student\cf6 \strokec6 .\cf8 \strokec8 exam\cf6 \strokec6  \cf2 \strokec2 *\cf6 \strokec6  \cf11 \strokec11 0.6\cf6 \strokec6 ;\cb1 \
-\cb3         \cf8 \strokec8 group\cf6 \strokec6 .\cf8 \strokec8 push_back\cf6 \strokec6 (\cf8 \strokec8 student\cf6 \strokec6 );\cb1 \
-\cb3         \cf8 \strokec8 student\cf6 \strokec6 .\cf8 \strokec8 grade\cf6 \strokec6 .\cf8 \strokec8 clear\cf6 \strokec6 ();\cb1 \
-\cb3     \}
-\f2\i \cf10 \strokec10 // galima priskirti grupej, kai turime A.rez; pushbackinam studento pavadinima
-\f0\i0 \cf6 \cb1 \strokec6 \
-\cb3 \}\cb1 \
-\pard\pardeftab720\partightenfactor0
-
-\f1\b \cf9 \cb3 \strokec9 void
-\f0\b0 \cf6 \strokec6  \cf8 \strokec8 output\cf6 \strokec6 (\cf7 \strokec7 vector\cf6 \strokec6 <\cf7 \strokec7 Students\cf6 \strokec6 >
-\f1\b \cf9 \strokec9 &
-\f0\b0 \cf6 \strokec6  group) \cb1 \
-\pard\pardeftab720\partightenfactor0
-\cf6 \cb3 \{\cb1 \
-\cb3     \cf2 \strokec2 for\cf6 \strokec6  (
-\f1\b \cf9 \strokec9 auto
-\f0\b0 \cf6 \strokec6  \cf8 \strokec8 student\cf6 \strokec6  : group) \{\cb1 \
-\cb3         \cf8 \strokec8 cout\cf6 \strokec6  \cf8 \strokec8 <<\cf6 \strokec6  \cf8 \strokec8 left\cf6 \strokec6  \cf8 \strokec8 <<\cf6 \strokec6  \cf8 \strokec8 setw\cf6 \strokec6 (\cf11 \strokec11 10\cf6 \strokec6 ) \cf8 \strokec8 <<\cf6 \strokec6  \cf8 \strokec8 student\cf6 \strokec6 .\cf8 \strokec8 name\cf6 \strokec6  \cf8 \strokec8 <<\cf6 \strokec6  \cf8 \strokec8 left\cf6 \strokec6  \cf8 \strokec8 <<\cf6 \strokec6  \cf8 \strokec8 setw\cf6 \strokec6 (\cf11 \strokec11 20\cf6 \strokec6 ) \cf8 \strokec8 <<\cf6 \strokec6  \cf8 \strokec8 student\cf6 \strokec6 .\cf8 \strokec8 last_name\cf6 \strokec6 ;\cb1 \
-\pard\pardeftab720\partightenfactor0
-
-\f2\i \cf10 \cb3 \strokec10         //for (auto k : A.paz) cout << setw(3) << k;
-\f0\i0 \cf6 \cb1 \strokec6 \
-
-\f2\i \cf10 \cb3 \strokec10         //cout << setw(5) << A.egz;
-\f0\i0 \cf6 \cb1 \strokec6 \
-\pard\pardeftab720\partightenfactor0
-\cf6 \cb3         \cf8 \strokec8 cout\cf6 \strokec6  \cf8 \strokec8 <<\cf6 \strokec6  \cf8 \strokec8 setw\cf6 \strokec6 (\cf11 \strokec11 10\cf6 \strokec6 ) \cf8 \strokec8 <<\cf6 \strokec6  \cf8 \strokec8 student\cf6 \strokec6 .\cf8 \strokec8 result\cf6 \strokec6  \cf8 \strokec8 <<\cf6 \strokec6  \cf8 \strokec8 endl\cf6 \strokec6 ;\cb1 \
-\cb3     \}\cb1 \
-\cb3 \}\cb1 \
+void input(vector<Students>& group) 
+{
+    for (int ii = 0; ii < 2; ii++) {
+        Students student;
+        cout << "Iveskite varda ir pavarde: ";
+        cin >> student.name >> student.last_name;
+        cout << "Iveskite semestro ivertinimus. Kiek ju bus? ";
+        int n, sum = 0;
+        cin >> n;
+        for (int i = 0; i < n; i++) 
+        {
+            int temp;
+            cout << "Iveskite " << i + 1 << " pazymi is " << n << ":";
+            cin >> temp;
+            student.grade.push_back(temp); sum += temp;
+        }
+        cout << "Iveskite egzamina: "; cin >> student.exam;
+        student.result = sum * 1.0 / (n * 1.0) * 0.4 + student.exam * 0.6;
+        group.push_back(student);
+        student.grade.clear();
+    }// galima priskirti grupej, kai turime A.rez; pushbackinam studento pavadinima
+}
+void output(vector<Students>& group) 
+{
+    for (auto student : group) {
+        cout << left << setw(10) << student.name << left << setw(20) << student.last_name;
+        //for (auto k : A.paz) cout << setw(3) << k;
+        //cout << setw(5) << A.egz;
+        cout << setw(10) << student.result << endl;
+    }
 }
